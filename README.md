0eae617ec3c34337c5ff1d7373abb04af7fac3fb
Antoine P <depertat@hotmail.fr>
2021-09-20 01:45:14 +0200

    Home with section done working on Header

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Tesla-clone
+<img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.carlogos.org%2Flogo%2FTesla-logo-2003-2500x2500.png&f=1&nofb=1" width="150" alt="Tesla logo"><em></em><span style='margin: 20px;'>X</span><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-download.com%2Fwp-content%2Fuploads%2F2016%2F09%2FReact_logo_wordmark.png&f=1&nofb=1" width="200" alt="React.js logo">
+
+<hr class="line"></br>
+
+👇 <em>Result here</em>👇 </br>
+https://netflix-clone-1fbfc.web.app
+
+</br></br></br></br>
+
+<h2>FRONTEND</h2></br>
+
+<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-download.com%2Fwp-content%2Fuploads%2F2016%2F09%2FReact_logo_wordmark.png&f=1&nofb=1" width="200" alt="React.js logo"></br></br></br></br></br>
+
+<h2>BACKEND</h2></br>
+
+<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fappdevcon.nl%2Fwp-content%2Fuploads%2F2019%2F02%2Flogo_lockup_firebase_horizontal.png&f=1&nofb=1" width="200" alt="Firebase logo"></br></br></br></br></br></br>
+
+<hr class="line"></br></br></br></br>
+
+<h2>Features</h2></br>
+
+👉 <em>React</em> hooks</br>
+👉 <em>React</em>redux</br>
+👉 <em>React</em> styled components</br>
+👉 <em>Firebase</em> hosting
+
+</br></br></br>
+
+<hr class="line"></br></br></br>
---
+++ tesla-clone/src/components/Section.js
@@ -0,0 +1,83 @@
+import React from "react";
+import styled from "styled-components";
+
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) {
+  return (
+    <Wrap bgImage={backgroundImg}>
+      <ItemText>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+          {/* hide for Section without rightBtnText */}
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
+    </Wrap>
+  );
+}
+
+export default Section;
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  height: 100vh;
+  width: 100vw;
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
+  background-size: cover;
+  background-position: center;
+  background-repear: no-repeat;
+`;
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const Buttons = styled.div``;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+`;
+const RightButton = styled(LeftButton)`
+  background: white;
+  opacity: 0.65;
+  color: black;
+`;
+
+const DownArrow = styled.img`
+  margin-top: 20px;
+  height: 40px;
+  overflow-x: hidden;
+  animation: animateDown infinite 1.5s;
+`;
